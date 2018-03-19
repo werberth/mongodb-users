1755a72750029e9aaa4df16a9728f90d883d2f2d
Werberth Vinícius <werberthvinicius@gmail.com>
2018-03-19 03:08:22 -0300

    Add beforeEach to clean all instances of User model on database before each test

--- test/test_helper.js
+++ test/test_helper.js
@@ -5,4 +5,11 @@ mongoose.connection
     .once('open', () => console.log('Good to go!'))
     .on('error', (error) => {
         console.warn('Warning', error)
+    });
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        // Ready to run the next test
+        done();
     })
+})
