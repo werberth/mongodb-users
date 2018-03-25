254a02c3de82a17a42c3c7543dbda1830bec49f1
Werberth Vinícius <werberthvinicius@gmail.com>
2018-03-25 02:08:54 -0300

    Add to find a user with a particular id

--- test/reading_test.js
+++ test/reading_test.js
@@ -10,10 +10,18 @@ describe('Reading users out of the database', () => {
             .then(() => done());
     });
 
-    it ('finds all user with a name of joe', (done) => {
+    it('finds all user with a name of joe', (done) => {
         User.find({ name: 'Joe' })
             .then((users) => {
                 assert(users[0]._id.toString() === joe._id.toString());
+                done();
+            });
+    });
+
+    it('find a user with a particular id', (done) => {
+        User.findOne({ _id: joe._id })
+            .then((user) => {
+                assert(user.name === 'Joe');
                 done();
             });
     });
