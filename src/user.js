519ade9207e65eb863b85670c27075367c943c74
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-15 16:48:50 -0300

    Nesting Posts on Users

--- src/user.js
+++ src/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
+const PostSchema = require('./post');
+const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-
-const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: {
@@ -13,7 +13,8 @@ const UserSchema = new Schema({
         },
         required: [true, 'Name is required.']
     },
-    postCount:Number
+    postCount:Number,
+    posts: [PostSchema]
 });
 
 const User = mongoose.model('user', UserSchema);
