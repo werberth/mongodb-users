2cfc947f18a086afeb0af19d656fffdb2e1c74bd
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-15 20:33:03 -0300

    Fix test crash errors

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe', postCount: 0 });
+        joe = new User({ name: 'Joe' });
         joe.save()
             .then(() => done());
     });
@@ -50,11 +50,11 @@ describe('Updating records', () => {
         );
     });
 
-    it('A user can have their postcount incremented by 1', (done) => {
-        User.update({ name: 'Joe' }, { $inc: { postCount: 10 } })
+    it('A user can have their likes incremented by 1', (done) => {
+        User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
             .then(() => User.findOne({ name: 'Joe' }))
             .then((user) => {
-                assert(user.postCount === 10);
+                assert(user.likes === 10);
                 done();
             });
     });
