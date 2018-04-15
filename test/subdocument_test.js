0a4a98949db585c79a7a911e91a0fbcd031e46cf
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-15 18:52:32 -0300

    Add "Can add subdocuments to an existing record" and "can remove an existing subdocument" tests

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -17,4 +17,45 @@ describe('Subdocuments', () => {
                 done();
             });
     });
+
+
+    it('Can add subdocuments to an existing record', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            posts: []
+        });
+
+        joe.save()
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                user.posts.push({ title: 'New Post' });
+                return user.save();
+            })
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user.posts[0].title === 'New Post');
+                done();
+            });
+    });
+
+
+    it('can remove an existing subdocument', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            'posts': [{ title: 'New Title' }]
+        });
+
+        joe.save()
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                const post = user.posts[0];
+                post.remove();
+                return user.save();
+            })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user.posts.length === 0);
+                done();
+            });
+    });
 });
