6c9e6fc21b92802892ea5ae5caf0a6581b89b486
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-16 02:23:25 -0300

    Add association_test.js test setup

--- test/association_test.js
+++ test/association_test.js
@@ -1,8 +1,23 @@
+import { Promise } from 'mongoose';
+
 const mongoose = require('mongoose');
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
 describe('Assocations', () => {
+    let joe, blogPost, comment;
 
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
+        blogPost = new BlogPost({ title: 'JS is Geat', content: 'Yep it really is' });
+        comment = new Comment({ content: 'Congrats on great post' });
+
+        joe.blogPost.push(blogPost);
+        blogPost.comments.push(comment);
+        comment.user = joe;
+
+        Promise.all([joe.save(), blogPost.save(), comment.save()])
+            .then(() => done());
+    });
 });
