3b993535644fd54eaa5558ca3134ea77e0d7a3fe
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-17 00:57:22 -0300

    Populating user.blogPosts Queries

--- test/association_test.js
+++ test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert')
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -20,9 +21,10 @@ describe('Assocations', () => {
   });
 
   it.only('saves a relation between a user and a blogpost', (done) => {
-    User.findOne({ name: 'Joe' }).then()
+    User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
       .then((user) => {
-        console.log(user);
+        assert(user.blogPosts[0].title === 'JS is Great');
         done();
       });
   });
