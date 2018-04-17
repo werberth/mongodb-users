682e3c0967f3566bbb2db42ad971c9e43ed5799b
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-16 23:29:01 -0300

    Promise.All for Parallel Operations

--- test/test_helper.js
+++ test/test_helper.js
@@ -10,8 +10,12 @@ before((done) => {
 });
 
 beforeEach((done) => {
-    mongoose.connection.collections.users.drop(() => {
-        // Ready to run the next test
-        done();
+    const { users, comments, blogposts } = mongoose.connection.collections;
+    users.drop(() => {
+        comments.drop(() => {
+            blogposts.drop(() => {
+                done();
+            });
+        });
     });
 });
