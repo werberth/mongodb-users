65fbf78de7487043f9741d9885bcdf13ab6aff7f
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-17 01:28:22 -0300

    Loading Deeply Nested Associations

--- test/association_test.js
+++ test/association_test.js
@@ -20,11 +20,33 @@ describe('Assocations', () => {
       .then(() => done());
   });
 
-  it.only('saves a relation between a user and a blogpost', (done) => {
+  it('saves a relation between a user and a blogpost', (done) => {
     User.findOne({ name: 'Joe' })
       .populate('blogPosts')
       .then((user) => {
         assert(user.blogPosts[0].title === 'JS is Great');
+        done();
+      });
+  });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
         done();
       });
   });
